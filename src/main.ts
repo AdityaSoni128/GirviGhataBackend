0ed0a6dae2312186338aab8546983a4933d1c3f2
Aditya Soni <showoff128@gmail.com>
2026-09-01 15:25:01 +0530

    fix production start path

--- src/main.ts
+++ src/main.ts
@@ -58,7 +58,7 @@ async function bootstrap() {
   );
 
   const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
   // eslint-disable-next-line no-console
   console.log(`Girvi Ghata backend listening on port ${port}`);
 }
